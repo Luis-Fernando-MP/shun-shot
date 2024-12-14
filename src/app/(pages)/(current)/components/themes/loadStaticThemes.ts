a373a164196a87ae8dd06ebce88fcd2a80ae87c7
Luis fernando JU <luigfmp@gmail.com>
2024-12-14 02:01:17 -0500

    feat: :art: monaco themes

---
+++ src/app/(pages)/(current)/components/themes/loadStaticThemes.ts
@@ -0,0 +1,155 @@
+import Active4D from 'monaco-themes/themes/Active4D.json'
+import AllHallowsEve from 'monaco-themes/themes/All Hallows Eve.json'
+import Amy from 'monaco-themes/themes/Amy.json'
+import BirdsOfParadise from 'monaco-themes/themes/Birds of Paradise.json'
+import Blackboard from 'monaco-themes/themes/Blackboard.json'
+import BrillianceBlack from 'monaco-themes/themes/Brilliance Black.json'
+import BrillianceDull from 'monaco-themes/themes/Brilliance Dull.json'
+import ChromeDevTools from 'monaco-themes/themes/Chrome DevTools.json'
+import Clouds from 'monaco-themes/themes/Clouds.json'
+import Cobalt from 'monaco-themes/themes/Cobalt.json'
+import Dracula from 'monaco-themes/themes/Dracula.json'
+import Dreamweaver from 'monaco-themes/themes/Dreamweaver.json'
+import Eiffel from 'monaco-themes/themes/Eiffel.json'
+import GitHubDark from 'monaco-themes/themes/GitHub Dark.json'
+import GitHubLight from 'monaco-themes/themes/GitHub Light.json'
+import GitHub from 'monaco-themes/themes/GitHub.json'
+import IDLE from 'monaco-themes/themes/IDLE.json'
+import LAZY from 'monaco-themes/themes/LAZY.json'
+import MerbivoreSoft from 'monaco-themes/themes/Merbivore Soft.json'
+import MonokaiBright from 'monaco-themes/themes/Monokai Bright.json'
+import NightOwl from 'monaco-themes/themes/Night Owl.json'
+import Nord from 'monaco-themes/themes/Nord.json'
+import OceanicNext from 'monaco-themes/themes/Oceanic Next.json'
+import PastelsOnDark from 'monaco-themes/themes/Pastels on Dark.json'
+import SlushAndPoppies from 'monaco-themes/themes/Slush and Poppies.json'
+import SolarizedLight from 'monaco-themes/themes/Solarized-light.json'
+import SpaceCadet from 'monaco-themes/themes/SpaceCadet.json'
+import Sunburst from 'monaco-themes/themes/Sunburst.json'
+import TextmateMacClassic from 'monaco-themes/themes/Textmate (Mac Classic).json'
+import TomorrowNightBlue from 'monaco-themes/themes/Tomorrow-Night-Blue.json'
+import TomorrowNightBright from 'monaco-themes/themes/Tomorrow-Night-Bright.json'
+import TomorrowNightEighties from 'monaco-themes/themes/Tomorrow-Night-Eighties.json'
+import TomorrowNight from 'monaco-themes/themes/Tomorrow-Night.json'
+import Tomorrow from 'monaco-themes/themes/Tomorrow.json'
+import Twilight from 'monaco-themes/themes/Twilight.json'
+import UpstreamSunburst from 'monaco-themes/themes/Upstream Sunburst.json'
+import VibrantInk from 'monaco-themes/themes/Vibrant Ink.json'
+import XcodeDefault from 'monaco-themes/themes/Xcode_default.json'
+import krTheme from 'monaco-themes/themes/krTheme.json'
+import Monoindustrial from 'monaco-themes/themes/monoindustrial.json'
+
+import amethystLight from './data/amethyst-light.json'
+import amethyst from './data/amethyst.json'
+import augustArstotzka from './data/august-arstotzka.json'
+import augustCatppuccin from './data/august-catppuccin.json'
+import augustOcean from './data/august-ocean.json'
+import bold from './data/bold.json'
+import contrast from './data/contrast.json'
+import csb from './data/csb.json'
+import decay from './data/decay.json'
+import dobriAmethyst from './data/dobri-amethyst.json'
+import dobriBlack from './data/dobri-black.json'
+import dobriIndigo from './data/dobri-indigo.json'
+import dobriOne from './data/dobri-one.json'
+import duoToneEarth from './data/duoTone-earth.json'
+import duoToneForest from './data/duoTone-forest.json'
+import duoToneSea from './data/duoTone-sea.json'
+import duoTone from './data/duoTone.json'
+import earthbound from './data/earthbound.json'
+import enfocado from './data/enfocado.json'
+import gruvboxLight from './data/gruvbox-light.json'
+import gruvbox from './data/gruvbox.json'
+import kimbie from './data/kimbie.json'
+import redefinedAyu from './data/redefined-ayu.json'
+import redefinedSnazzy from './data/redefined-snazzy.json'
+import tinaciousBlack from './data/tinacious-black.json'
+import tinaciousLight from './data/tinacious-light.json'
+import tinacious from './data/tinacious.json'
+import viowArabian from './data/viow-arabian.json'
+import viowMars from './data/viow-mars.json'
+import viowNeon from './data/viow-neon.json'
+import vsDark from './data/vs-dark.json'
+import vsLight from './data/vs-light.json'
+
+export const themesData: any = {
+  redefinedSnazzy,
+  redefinedAyu,
+  earthbound,
+  amethyst,
+  augustCatppuccin,
+  augustArstotzka,
+  augustOcean,
+  viowArabian,
+  viowMars,
+  viowNeon,
+  dobriOne,
+  dobriIndigo,
+  tinacious,
+  decay,
+  enfocado,
+  bold,
+  csb,
+  'vs-dark': vsDark,
+  Dracula,
+  GitHubDark,
+
+  MerbivoreSoft,
+  Nord,
+  PastelsOnDark,
+  contrast,
+  SpaceCadet,
+  Sunburst,
+  TomorrowNightBright,
+  TomorrowNightEighties,
+  TomorrowNight,
+  Twilight,
+  UpstreamSunburst,
+  VibrantInk,
+  krTheme,
+  Monoindustrial,
+
+  // Black
+  dobriBlack,
+  tinaciousBlack,
+  AllHallowsEve,
+  BrillianceDull,
+  BrillianceBlack,
+  gruvbox,
+  MonokaiBright,
+  kimbie,
+  BirdsOfParadise,
+
+  // specials
+  Cobalt,
+  Blackboard,
+  dobriAmethyst,
+  Amy,
+  OceanicNext,
+  NightOwl,
+  TomorrowNightBlue,
+  duoTone,
+  duoToneSea,
+  duoToneEarth,
+  duoToneForest,
+
+  // light theme
+  amethystLight,
+  XcodeDefault,
+  GitHub,
+  tinaciousLight,
+  'vs-light': vsLight,
+  IDLE,
+  gruvboxLight,
+  LAZY,
+  SolarizedLight,
+  SlushAndPoppies,
+  TextmateMacClassic,
+  Tomorrow,
+  GitHubLight,
+  Dreamweaver,
+  Eiffel,
+  ChromeDevTools,
+  Clouds,
+  Active4D
+}
---
+++ src/app/(pages)/(current)/components/themes/monaco-type.ts
@@ -0,0 +1,31 @@
+type ThemeRule = {
+  background?: string
+  foreground?: string
+  fontStyle?: 'italic' | 'bold' | 'underline'
+  token: string
+}
+
+type ThemeColors = {
+  'editor.foreground': string
+  'editor.background': string
+  'editor.selectionBackground': string
+  'editor.lineHighlightBackground': string
+  'editorCursor.foreground': string
+  'editorWhitespace.foreground': string
+}
+
+type Theme = {
+  base: string
+  inherit: boolean
+  rules: ThemeRule[]
+  colors: ThemeColors
+}
+
+export type ThemesMonaco = {
+  base: string
+  inherit: boolean
+  rules: ThemeRule[]
+  colors: ThemeColors
+  default: Theme
+  // name: string
+}
