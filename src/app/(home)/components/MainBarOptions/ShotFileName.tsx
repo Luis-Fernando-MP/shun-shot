9de873479ade33352d66a6551b1fac723aff0556
Juls <luigfmp@gmail.com>
2025-03-16 20:32:36 -0500

    feat: :sparkles: add image download functionality to ShotFileName component with useDownloadImage hook

--- src/app/(home)/components/MainBarOptions/ShotFileName.tsx
+++ src/app/(home)/components/MainBarOptions/ShotFileName.tsx
@@ -1,15 +1,32 @@
+import useDownloadImage from '@/app/hooks/useDownloadImage'
+import { acl } from '@/shared/acl'
 import IconButton from '@/shared/ui/IconButton'
 import LabelText from '@/shared/ui/LabelText'
 import { CloudDownload } from 'lucide-react'
-import type { FC } from 'react'
+import { type FC } from 'react'
 
 import useCodeShotStore from '../../store/codeShot.store'
 
 const ShotFileName: FC = () => {
   const { fileName } = useCodeShotStore()
 
+  const { downloadPngImage, isDownloading } = useDownloadImage()
+
+  const handleDownload = async () => {
+    const $monacoEditor = document.getElementById('monacoEditor')
+    if (!($monacoEditor instanceof HTMLElement)) return
+
+    downloadPngImage({ fileName, $element: $monacoEditor, scaleFactor: 5 })
+  }
+
   return (
-    <IconButton label='Formatear código' transparent outline className='mainBar-fileName'>
+    <IconButton
+      label='Formatear código'
+      transparent
+      outline
+      className={`mainBar-fileName ${acl(isDownloading, 'loading')}`}
+      onClick={handleDownload}
+    >
       <CloudDownload />
       <p>{fileName}</p>
       <LabelText>.png</LabelText>
---
+++ src/app/hooks/useDownloadImage.tsx
@@ -0,0 +1,57 @@
+import { toaster } from '@/shared/components/Toast'
+import domToImageMore from 'dom-to-image-more'
+import { useCallback, useState } from 'react'
+import toast from 'react-hot-toast'
+
+interface HandleDownloadProps {
+  fileName: string
+  $element: HTMLElement
+  scaleFactor: number
+}
+
+const useDownloadImage = () => {
+  const [isDownloading, setIsDownloading] = useState(false)
+
+  const downloadPngImage = useCallback(
+    async ({ fileName, $element, scaleFactor }: HandleDownloadProps) => {
+      if (isDownloading) return
+      const toastId = toaster({ title: 'Procesando imagen...', type: 'pending' })
+      setIsDownloading(true)
+
+      try {
+        const dataUrl = await domToImageMore.toPng($element, {
+          quality: 1,
+          width: $element.offsetWidth * scaleFactor,
+          height: $element.offsetHeight * scaleFactor,
+          style: {
+            transform: `scale(${scaleFactor})`,
+            transformOrigin: 'top left',
+            borderRadius: 0
+          }
+        })
+
+        const link = document.createElement('a')
+        link.download = `${fileName}.png`
+        link.href = dataUrl
+        link.click()
+        toaster({ title: 'Completado', type: 'success', id: toastId })
+      } catch (error) {
+        console.log(error)
+        toaster({ title: 'La descarga ha fallado', type: 'error', description: 'Por favor, inténtalo de nuevo', id: toastId })
+      } finally {
+        setTimeout(() => {
+          toast.dismiss(toastId)
+        }, 3000)
+        setIsDownloading(false)
+      }
+    },
+    [setIsDownloading]
+  )
+
+  return {
+    isDownloading,
+    downloadPngImage
+  }
+}
+
+export default useDownloadImage
